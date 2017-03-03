885ccaee89f54ec089081413481bc798013a0a0f
Shayan Salehian <shayan72@gmail.com>
2017-03-02 16:43:04 -0800

    add spark and kafka compatible libraries and test integration of spark streaming and kafka

--- build.sbt
+++ build.sbt
@@ -2,9 +2,20 @@ name := "Twitter_Language_Identification"
 
 version := "1.0"
 
-scalaVersion := "2.12.1"
+scalaVersion := "2.11.8"
 
 libraryDependencies ++= Seq(
+  "org.scala-lang" % "scala-compiler" % "2.11.8",
   "org.twitter4j" % "twitter4j-core" % "3.0.3",
-  "org.twitter4j" % "twitter4j-stream" % "3.0.3"
+  "org.twitter4j" % "twitter4j-stream" % "3.0.3",
+  "org.apache.kafka" % "kafka_2.11" % "0.10.2.0"
+    exclude("javax.jms", "jms")
+    exclude("com.sun.jdmk", "jmxtools")
+    exclude("com.sun.jmx", "jmxri")
+    exclude("org.slf4j", "slf4j-simple"),
+  "org.apache.spark" % "spark-core_2.11" % "2.1.0",
+  "org.apache.spark" % "spark-streaming_2.11" % "2.1.0",
+  "org.apache.spark" % "spark-streaming-kafka-0-10_2.11" % "2.1.0",
+  "jline" % "jline" % "2.12.1"
+
 )
--- src/main/scala/test.scala
+++ src/main/scala/test.scala
@@ -1,4 +1,17 @@
 import twitter4j._
+import kafka._
+import kafka.serializer.StringDecoder
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.kafka010._
+import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
+import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
+import org.apache.log4j.Logger
+import org.apache.log4j.Level
 
 object test {
 
@@ -11,21 +24,81 @@ object test {
       .build
 
     def simpleStatusListener = new StatusListener() {
-      def onStatus(status: Status) { println(status.getText) }
+      def onStatus(status: Status) {
+        println(status.getText)
+        //        // Spark test
+        //        val logFile = "README.md" // Should be some file on your system
+        //        val conf = new SparkConf().setAppName("Twitter Language Identification").setMaster("local[2]").set("spark.executor.memory","1g")
+        //        val sc = new SparkContext(conf)
+        //        val logData = sc.textFile(logFile, 2).cache()
+        //        val numAs = logData.filter(line => line.contains("a")).count()
+        //        val numBs = logData.filter(line => line.contains("b")).count()
+        //        println(s"Lines with a: $numAs, Lines with b: $numBs")
+        //        sc.stop()
+      }
+
       def onDeletionNotice(statusDeletionNotice: StatusDeletionNotice) {}
+
       def onTrackLimitationNotice(numberOfLimitedStatuses: Int) {}
-      def onException(ex: Exception) { ex.printStackTrace }
+
+      def onException(ex: Exception) {
+        ex.printStackTrace
+      }
+
       def onScrubGeo(arg0: Long, arg1: Long) {}
+
       def onStallWarning(warning: StallWarning) {}
     }
   }
 
   def main(args: Array[String]): Unit = {
-    val twitterStream = new TwitterStreamFactory(Util.config).getInstance
-    twitterStream.addListener(Util.simpleStatusListener)
-    twitterStream.sample
-//    Thread.sleep(10000)
-//    twitterStream.cleanUp
-//    twitterStream.shutdown
+
+    Logger.getLogger("org").setLevel(Level.OFF)
+    Logger.getLogger("akka").setLevel(Level.OFF)
+
+
+    //    val twitterStream = new TwitterStreamFactory(Util.config).getInstance
+    //    twitterStream.addListener(Util.simpleStatusListener)
+    //    twitterStream.sample
+
+
+    // Twitter Stream
+    //    val twitterStream = new TwitterStreamFactory(Util.config).getInstance
+    //    twitterStream.addListener(Util.simpleStatusListener)
+    //    twitterStream.sample
+    //    Thread.sleep(10000)
+    //    twitterStream.cleanUp
+    //    twitterStream.shutdown
+
+
+    //    // Spark test
+    //    val logFile = "README.md" // Should be some file on your system
+    //    val conf = new SparkConf().setAppName("Twitter Language Identification").setMaster("local[2]").set("spark.executor.memory","1g")
+    //    val sc = new SparkContext(conf)
+    //    val logData = sc.textFile(logFile, 2).cache()
+    //    val numAs = logData.filter(line => line.contains("a")).count()
+    //    val numBs = logData.filter(line => line.contains("b")).count()
+    //    println(s"Lines with a: $numAs, Lines with b: $numBs")
+    //    sc.stop()
+
+    // Spark Streaming + Kafka Integration
+    val ssc = new StreamingContext("local[*]", "test", Seconds(1))
+
+    val kafkaParams = Map("bootstrap.servers" -> "localhost:9092",
+      "key.deserializer" -> classOf[StringDeserializer],
+      "value.deserializer" -> classOf[StringDeserializer],
+      "group.id" -> "use_a_separate_group_id_for_each_stream",
+      "auto.offset.reset" -> "latest",
+      "enable.auto.commit" -> (false: java.lang.Boolean))
+
+    val topics = List("twitter-topic").toSet
+    val lines = KafkaUtils.createDirectStream[String, String](ssc, PreferConsistent, Subscribe[String, String](topics, kafkaParams)).map(record => record.value)
+
+    val wordCounts = lines.map(line => line.split(" ").size).map(word => (word, 1)).reduceByKey((a, b) => a + b)
+    wordCounts.print()
+
+    ssc.start()
+    ssc.awaitTermination()
+
   }
 }
