86f21df7a898b9632f034f8a1366e7537961d066
Shayan Salehian <shayan72@gmail.com>
2017-03-21 19:14:20 -0700

    Added basic evaluation of distributed k-means algorithm

--- src/main/scala/DistributedLanguageDetection.scala
+++ src/main/scala/DistributedLanguageDetection.scala
@@ -8,6 +8,8 @@ import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
 import org.apache.spark.streaming.kafka010._
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 
+import scala.collection.mutable.HashMap
+
 
 object DistributedLanguageDetection {
 
@@ -18,7 +20,7 @@ object DistributedLanguageDetection {
     Logger.getLogger("org").setLevel(Level.OFF)
     Logger.getLogger("akka").setLevel(Level.OFF)
 
-//    generate_tweets()
+    //    generate_tweets()
     distributed_kmeans()
   }
 
@@ -32,9 +34,13 @@ object DistributedLanguageDetection {
     val conf = new SparkConf().setAppName("DistributedLanguageDetection").setMaster("local[*]")
     val sc = new SparkContext(conf)
 
+    var langs_arr = Array.fill[HashMap[String, Int]](3)(HashMap[String, Int]())
+
     // Load and parse the data
     val data = sc.textFile(stored_tweets_file_input)
     val parsedData = data.map(getText).map(s => Vectors.dense(TextProcessing.create_feature_vector(s))).cache()
+
+    val tweets = data.collect().toList
 
     // Cluster the data into two classes using KMeans
     val numClusters = 3
@@ -46,8 +52,40 @@ object DistributedLanguageDetection {
     println("Within Set Sum of Squared Errors = " + WSSSE)
 
     // Save and load model
-    clusters.save(sc, "target/org/apache/spark/KMeansExample/KMeansModel")
-    val sameModel = KMeansModel.load(sc, "target/org/apache/spark/KMeansExample/KMeansModel")
+    clusters.save(sc, "output/KMeansModel")
+    val sameModel = KMeansModel.load(sc, "output/KMeansModel")
+
+    // Evaluate
+    tweets.foreach { tweet =>
+      val predicted_class = sameModel.predict(Vectors.dense(TextProcessing.create_feature_vector(getText(tweet))))
+      val language = getLanguage(tweet)
+
+      if (langs_arr(predicted_class) contains language)
+        langs_arr(predicted_class)(language) += 1
+      else
+        langs_arr(predicted_class) += (language -> 1)
+    }
+
+    for (lang_arr <- langs_arr) {
+      var total = 0
+      var max = Integer.MIN_VALUE
+      var max_lang = ""
+
+      lang_arr.foreach { tuple =>
+        val language = tuple._1
+        val count = tuple._2
+
+        total += count
+        if (count > max) {
+          max = count
+          max_lang = language
+        }
+      }
+
+      if (total > 0)
+        println("dominant language: " + max_lang + " --> accuracy: " + max.toFloat / total + " , max = " + max + ", total = " + total)
+    }
+    println("===========")
 
     sc.stop()
   }
@@ -58,5 +96,13 @@ object DistributedLanguageDetection {
       splitted(1)
     else
       splitted(0)
+  }
+
+  def getLanguage(string: String): String = {
+    val splitted = string.split(", ", 2)
+    if (splitted.size == 2)
+      splitted(0)
+    else
+      ""
   }
 }
